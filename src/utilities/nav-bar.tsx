893c991bfd6585e42e88b78103087f2af57ea7cd
Angel Ugarte <fredanup@hotmail.com>
2024-07-16 18:24:28 -0500

    Eliminación de opción de menú

--- src/utilities/nav-bar.tsx
+++ src/utilities/nav-bar.tsx
@@ -38,18 +38,6 @@ export default function NavBar() {
         <p className="text-gray-500 text-sm cursor-pointer">Convocatorias</p>
       </Link>
 
-      <Link
-        href={'/dashboard/callingManagement'}
-        className="flex flex-col items-center md:flex md:flex-row md:gap-1"
-      >
-        <svg
-          viewBox="0 0 640 512"
-          className={`h-8 w-8 cursor-pointer fill-gray-500 p-1.5  `}
-        >
-          <path d="M32 32c17.7 0 32 14.3 32 32V400c0 8.8 7.2 16 16 16H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H80c-44.2 0-80-35.8-80-80V64C0 46.3 14.3 32 32 32zM160 224c17.7 0 32 14.3 32 32v64c0 17.7-14.3 32-32 32s-32-14.3-32-32V256c0-17.7 14.3-32 32-32zm128-64V320c0 17.7-14.3 32-32 32s-32-14.3-32-32V160c0-17.7 14.3-32 32-32s32 14.3 32 32zm64 32c17.7 0 32 14.3 32 32v96c0 17.7-14.3 32-32 32s-32-14.3-32-32V224c0-17.7 14.3-32 32-32zM480 96V320c0 17.7-14.3 32-32 32s-32-14.3-32-32V96c0-17.7 14.3-32 32-32s32 14.3 32 32z" />
-        </svg>
-        <p className="text-gray-500 text-sm cursor-pointer">Reportes</p>
-      </Link>
       {!currentUser?.role?.match('applicant') && (
         <Link
           href={'/dashboard/users'}
