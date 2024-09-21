40757e5344f56e0e0f00f3e7f627ec8fc3a44c7b
fredanup <fredanup@gmail.com>
2024-09-21 12:38:41 -0500

    Estilizado visual

--- src/pages/dashboard/products.tsx
+++ src/pages/dashboard/products.tsx
@@ -71,23 +71,35 @@ export default function Products() {
           <FormTitle text="Productos" />
           <div className="flex flex-row gap-2 md:gap-4 ml-auto">
             <button
-              className="bg-sky-400 rounded-lg px-3 py-2 text-white text-sm cursor-pointer"
-              onClick={(event) => {
-                event.stopPropagation();
-                openMovementModal();
-              }}
-            >
-              Inventario
-            </button>
-
-            <button
-              className="bg-purple-400 rounded-lg px-3 py-2 text-white text-sm cursor-pointer"
+              className="rounded-lg border border-gray-200 px-3 py-2 gap-2 text-gray-500 text-sm cursor-pointer flex flex-row items-center"
               onClick={(event) => {
                 event.stopPropagation();
                 openEditModal(null);
               }}
             >
-              Agregar
+              <svg
+                viewBox="0 0 576 512"
+                className={`h-4 w-4 cursor-pointer fill-gray-500`}
+              >
+                <path d="M547.6 103.8L490.3 13.1C485.2 5 476.1 0 466.4 0L109.6 0C99.9 0 90.8 5 85.7 13.1L28.3 103.8c-29.6 46.8-3.4 111.9 51.9 119.4c4 .5 8.1 .8 12.1 .8c26.1 0 49.3-11.4 65.2-29c15.9 17.6 39.1 29 65.2 29c26.1 0 49.3-11.4 65.2-29c15.9 17.6 39.1 29 65.2 29c26.2 0 49.3-11.4 65.2-29c16 17.6 39.1 29 65.2 29c4.1 0 8.1-.3 12.1-.8c55.5-7.4 81.8-72.5 52.1-119.4zM499.7 254.9c0 0 0 0-.1 0c-5.3 .7-10.7 1.1-16.2 1.1c-12.4 0-24.3-1.9-35.4-5.3L448 384l-320 0 0-133.4c-11.2 3.5-23.2 5.4-35.6 5.4c-5.5 0-11-.4-16.3-1.1l-.1 0c-4.1-.6-8.1-1.3-12-2.3L64 384l0 64c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-64 0-131.4c-4 1-8 1.8-12.3 2.3z" />
+              </svg>
+              <p>Producto</p>
+            </button>
+
+            <button
+              className="rounded-lg border border-gray-200 px-2 py-2 gap-2 text-gray-500 text-sm cursor-pointer flex flex-row items-center"
+              onClick={(event) => {
+                event.stopPropagation();
+                openMovementModal();
+              }}
+            >
+              <svg
+                viewBox="0 0 512 512"
+                className={`h-4 w-4 cursor-pointer fill-gray-500`}
+              >
+                <path d="M190.5 68.8L225.3 128l-1.3 0-72 0c-22.1 0-40-17.9-40-40s17.9-40 40-40l2.2 0c14.9 0 28.8 7.9 36.3 20.8zM64 88c0 14.4 3.5 28 9.6 40L32 128c-17.7 0-32 14.3-32 32l0 64c0 17.7 14.3 32 32 32l448 0c17.7 0 32-14.3 32-32l0-64c0-17.7-14.3-32-32-32l-41.6 0c6.1-12 9.6-25.6 9.6-40c0-48.6-39.4-88-88-88l-2.2 0c-31.9 0-61.5 16.9-77.7 44.4L256 85.5l-24.1-41C215.7 16.9 186.1 0 154.2 0L152 0C103.4 0 64 39.4 64 88zm336 0c0 22.1-17.9 40-40 40l-72 0-1.3 0 34.8-59.2C329.1 55.9 342.9 48 357.8 48l2.2 0c22.1 0 40 17.9 40 40zM32 288l0 176c0 26.5 21.5 48 48 48l144 0 0-224L32 288zM288 512l144 0c26.5 0 48-21.5 48-48l0-176-192 0 0 224z" />
+              </svg>
+              <p>Ejemplar</p>
             </button>
           </div>
         </div>
