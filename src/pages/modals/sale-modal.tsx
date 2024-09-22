4baf751f0aab30fb69698b5ca00f688ace44c4f6
fredanup <fredanup@gmail.com>
2024-09-21 19:21:57 -0500

    Estilizado final

--- src/pages/modals/sale-modal.tsx
+++ src/pages/modals/sale-modal.tsx
@@ -10,59 +10,60 @@ export default function SaleModal({
   onClose: () => void;
   saleId: string;
 }) {
-  /**
-   * Consultas a base de datos
-   */
-
   //Obtener todos los ejemplares vendidos
   const { data: userSales } = trpc.example.findSoldExamples.useQuery(saleId);
-
+  //Estilizado del fondo detrás del modal. Evita al usuario salirse del modal antes de elegir alguna opción
+  const overlayClassName = isOpen
+    ? 'fixed top-0 left-0 w-full h-full rounded-lg bg-gray-800 opacity-60 z-20'
+    : 'hidden';
   if (!isOpen) {
     return null; // No renderizar el modal si no está abierto
   }
 
   return (
     <>
-      <div className="fixed inset-0 flex items-center justify-center z-30">
-        <form className="w-11/12 md:w-1/2 flex flex-col gap-2 rounded-lg bg-white p-6 drop-shadow-lg">
+      {/* Fondo borroso y no interactivo */}
+      <div className={overlayClassName}></div>
+      <form className="fixed inset-0 z-30 flex items-center justify-center p-4 mb-4">
+        <div className="max-h-[calc(100%-5rem)] overflow-y-auto flex flex-col gap-2 rounded-lg bg-white p-6 drop-shadow-lg">
           <FormTitle text="Detalle de la venta" />
-
           <div className="overflow-x-auto">
-            <table className="w-full table-auto">
-              <thead className="border-b border-gray-200 text-left text-black text-sm font-light">
+            <table className="min-w-full table-auto border-collapse rounded-lg shadow-md overflow-hidden">
+              <thead className="bg-gray-100 text-left text-black font-medium">
                 <tr>
-                  <th className="py-4 pr-2">Código</th>
-                  <th className="py-4 pr-2">Producto</th>
-                  <th className="py-4 pr-2">Laboratorio</th>
-                  <th className="py-4 pr-2">Presentación</th>
-                  <th className="py-4 pr-2">Cantidad</th>
-                  <th className="py-4 pr-2">Precio</th>
+                  <th className="py-4 pX-6">Código</th>
+                  <th className="py-4 pX-6">Producto</th>
+                  <th className="py-4 pX-6">Laboratorio</th>
+                  <th className="py-4 pX-6">Presentación</th>
+                  <th className="py-4 pX-6">Cantidad</th>
+                  <th className="py-4 pX-6">Precio</th>
                 </tr>
               </thead>
-              <tbody>
+              <tbody className="bg-white text-gray-700">
                 {userSales?.map((sale, index) => (
                   <>
                     <tr
-                      className="border-b border-gray-200 text-sm font-light"
+                      className={`border-b transition-colors duration-200 ${
+                        index % 2 === 0 ? 'bg-gray-50' : ''
+                      } hover:bg-gray-100`}
                       key={index}
                     >
-                      <td className="py-4 pr-2">{sale.id}</td>
-                      <td className="py-4 pr-2">{sale.Product?.name}</td>
-                      <td className="py-4 pr-2">
+                      <td className="py-4 pX-6">{sale.id}</td>
+                      <td className="py-4 pX-6">{sale.Product?.name}</td>
+                      <td className="py-4 pX-6">
                         {sale.Product?.Laboratory?.name}
                       </td>
-                      <td className="py-4 pr-2">
+                      <td className="py-4 pX-6">
                         {sale.Product?.Presentation?.presentation}
                       </td>
-                      <td className="py-4 pr-2">{sale.Product?.quantity}</td>
-                      <td className="py-4 pr-2">{sale.Product?.price}</td>
+                      <td className="py-4 pX-6">{sale.Product?.quantity}</td>
+                      <td className="py-4 pX-6">{sale.Product?.price}</td>
                     </tr>
                   </>
                 ))}
               </tbody>
             </table>
           </div>
-
           <div className="mt-4 pt-4 flex flex-row justify-end gap-2 border-t border-gray-200">
             <button
               type="button"
@@ -72,8 +73,8 @@ export default function SaleModal({
               Cancelar
             </button>
           </div>
-        </form>
-      </div>
+        </div>
+      </form>
     </>
   );
 }
